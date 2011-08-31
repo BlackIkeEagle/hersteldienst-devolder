cab070702041236229e5e5aaa89354be3ffd8021
BlackEagle <ike.devolder@gmail.com>
2011-08-31 18:31:25 +0200

    404 hersteldienst enhanced
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- site/Pages/404.php
+++ site/Pages/404.php
@@ -6,11 +6,18 @@ class Pages_404 extends APages{
 	public function index() {
 		if($this->pageType == IPages::XHTML) {
 			header("HTTP/1.0 404 Not Found");
-			$this->document->setTitleAppend('OoOPS');
+			$this->document->setTitleAppend('OePS');
 			$content = $this->document->getElementById('content');
+			$img = $this->document->createElement('img');
+			$img->setAttribute('src', 'public/images/404.png');
+			$img->setAttribute('alt', '404');
+			$img->setAttribute('class', 'center');
+			$content->appendChild($img);
+
 			$err = $this->document->createElement('p', 'Oeps, de pagina die u wenst te bezoeken is niet gevonden');
 			$err->setAttribute('class', 'center hersteldienstfail');
 			$content->appendChild($err);
+
 			$link = $this->document->createElement('a', 'terug naar home');
 			$link->setAttribute('href', $this->basepath);
 			$link->setAttribute('class', 'center');
