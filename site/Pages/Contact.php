c35a9ef8b5251f74dbf75e21f7cb4a8a939afefd
BlackEagle <ike.devolder@gmail.com>
2011-09-04 14:50:19 +0200

    Contact :: added route
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- site/Pages/Contact.php
+++ site/Pages/Contact.php
@@ -1,21 +1,25 @@
 <?php
 class Pages_Contact extends APages {
+	private $content;
+
+	public function init() {
+		if($this->pageType == IPages::XHTML) {
+			$this->document->setTitleAppend('contact');
+			$this->document->addCss('public/css/contact.css');
+			$this->content = $this->document->getElementById('content');
+		}
+	}
 
 	/**
 	 * {@inheritdoc}
 	 */
 	public function index() {
 		if($this->pageType == IPages::XHTML) {
-			$this->document->setTitleAppend('contact');
-			$this->document->addCss('public/css/contact.css');
-
-			$content = $this->document->getElementById('content');
-
 			$title = $this->document->createElement(
 				'h1',
 				'Contact'
 			);
-			$content->appendChild($title);
+			$this->content->appendChild($title);
 			$table = $this->document->createElement('table');
 
 			$row = $this->document->createElement('tr');
@@ -60,28 +64,62 @@ class Pages_Contact extends APages {
 			$row->appendChild($element);
 			$table->appendChild($row);
 
-			$content->appendChild($table);
+			$this->content->appendChild($table);
+
+			$links = $this->document->createElement('p');
+			$routeLink = $this->document->createElement('a', 'route');
+			$routeLink->setAttribute('href', $this->basepath.'/contact/route');
+			$routeLink->setAttribute('title', 'route');
+			$links->appendChild($routeLink);
+			$this->content->appendChild($links);
 
 			$titleHours = $this->document->createElement(
 				'h2',
 				'Openingsuren'
 			);
-			$content->appendChild($titleHours);
+			$this->content->appendChild($titleHours);
 			$hours = $this->document->createElement(
 				'p',
 				'Ma - Vr: 8.00u tot 20.00u'
 			);
-			$content->appendChild($hours);
+			$this->content->appendChild($hours);
 			$hours = $this->document->createElement(
 				'p',
 				'Za : 8.00u tot 17.30u'
 			);
-			$content->appendChild($hours);
+			$this->content->appendChild($hours);
 			$hours = $this->document->createElement(
 				'p',
 				'Zondag gesloten'
 			);
-			$content->appendChild($hours);
+			$this->content->appendChild($hours);
+		}
+	}
+
+	public function route() {
+		if($this->pageType == IPages::XHTML) {
+			$this->document->setTitleAppend($this->document->getTitleAppend().' - route');
+			$title = $this->document->createElement(
+				'h1',
+				'Route'
+			);
+			$this->content->appendChild($title);
+
+			$googleContent = $this->document->createElement('iframe');
+			$googleContent->setAttribute('width', 780);
+			$googleContent->setAttribute('height', 600);
+			$googleContent->setAttribute('frameborder', 0);
+			$googleContent->setAttribute('scrolling', 'no');
+			$googleContent->setAttribute('marginheight', 0);
+			$googleContent->setAttribute('marginwidth', 0);
+			$googleContent->setAttribute('src', 'http://maps.google.be/maps?f=q&source=s_q&hl=nl&geocode=&q=11+Pieter+Paul+Rubensstraat,+Oostkamp,+Vlaams+Gewest&aq=t&sll=51.137552,3.228425&sspn=0.011646,0.03298&vpsrc=0&ie=UTF8&hq=&hnear=Pieter+Paul+Rubensstraat+11,+8020+Oostkamp,+West-Vlaanderen,+Vlaams+Gewest&ll=51.137571,3.228436&spn=0.026927,0.066948&z=14&iwloc=A&output=embed');
+			$this->content->appendChild($googleContent);
+			//$googleContent = $this->document->createElement('small');
+			//$googleLink = $this->document->createElement('a', 'Grotere kaart weergeven');
+			//$googleLink->setAttribute('href', 'http://maps.google.be/maps?f=q&source=embed&hl=nl&geocode=&q=11+Pieter+Paul+Rubensstraat,+Oostkamp,+Vlaams+Gewest&aq=1&sll=50.805935,4.432983&sspn=6.006454,16.885986&vpsrc=0&ie=UTF8&hq=&hnear=Pieter+Paul+Rubensstraat+11,+8020+Oostkamp,+West-Vlaanderen,+Vlaams+Gewest&z=14&ll=51.137552,3.228425');
+			//$googleLink->setAttribute('style', 'color:#0000FF;text-align:left');
+			//$googleContent->appendChild($googleLink);
+			//$this->content->appendChild($googleContent);
 		}
 	}
 }
