d4d71d0fa94d47b01201c0843b40b03db151e46d
BlackEagle <ike.devolder@gmail.com>
2011-10-22 19:22:26 +0200

    Contact :: updated link to google maps to show the company name
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- site/Pages/Contact.php
+++ site/Pages/Contact.php
@@ -117,14 +117,8 @@ class Pages_Contact extends APages {
 			$googleContent->setAttribute('scrolling', 'no');
 			$googleContent->setAttribute('marginheight', 0);
 			$googleContent->setAttribute('marginwidth', 0);
-			$googleContent->setAttribute('src', 'http://maps.google.be/maps?f=q&source=s_q&hl=nl&geocode=&q=11+Pieter+Paul+Rubensstraat,+Oostkamp,+Vlaams+Gewest&aq=t&sll=51.137552,3.228425&sspn=0.011646,0.03298&vpsrc=0&ie=UTF8&hq=&hnear=Pieter+Paul+Rubensstraat+11,+8020+Oostkamp,+West-Vlaanderen,+Vlaams+Gewest&ll=51.137571,3.228436&spn=0.026927,0.066948&z=14&iwloc=A&output=embed');
+			$googleContent->setAttribute('src', 'http://maps.google.be/maps?f=q&source=s_q&hl=nl&geocode=&q=hersteldienst+devolder&aq=&sll=50.805935,4.432983&sspn=6.020348,16.885986&vpsrc=6&ie=UTF8&hq=hersteldienst+devolder&hnear=&t=m&ll=51.137551,3.228425&spn=0.0227,0.065961&z=14&iwloc=A&cid=7808494652277294816&output=embed');
 			$this->content->appendChild($googleContent);
-			//$googleContent = $this->document->createElement('small');
-			//$googleLink = $this->document->createElement('a', 'Grotere kaart weergeven');
-			//$googleLink->setAttribute('href', 'http://maps.google.be/maps?f=q&source=embed&hl=nl&geocode=&q=11+Pieter+Paul+Rubensstraat,+Oostkamp,+Vlaams+Gewest&aq=1&sll=50.805935,4.432983&sspn=6.006454,16.885986&vpsrc=0&ie=UTF8&hq=&hnear=Pieter+Paul+Rubensstraat+11,+8020+Oostkamp,+West-Vlaanderen,+Vlaams+Gewest&z=14&ll=51.137552,3.228425');
-			//$googleLink->setAttribute('style', 'color:#0000FF;text-align:left');
-			//$googleContent->appendChild($googleLink);
-			//$this->content->appendChild($googleContent);
 		}
 	}
 
